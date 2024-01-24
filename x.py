dfdc0d697d7e64ceeb2894c96e0dea6ec5352a59
Jakub Myrta <mkuba2006@gmail.com>
2024-01-24 21:44:34 +0100

    24.01

---
+++ x.py
@@ -0,0 +1,12 @@
+bn = bin(10111)[2:]
+os = oct(123514265)[2:]
+szes = hex(1599)[2:]
+
+
+# bn = int("01010111", 2)
+# os = int('1712641',8)
+# szes = int("ABABCD",16)
+
+print("2 :", bn)
+print("8 :", os)
+print("16 :", szes)
